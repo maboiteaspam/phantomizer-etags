1fc45aef3b6341cc3cabc1fabd7c99746e582a5c
clement <maboiteaspam@wanadoo.fr>
2014-02-09 01:02:58 +0800

    Improved console output support Add more extension to detect text files Add progress dependency

--- tasks/build.js
+++ tasks/build.js
@@ -2,16 +2,15 @@
 
 module.exports = function(grunt) {
 
-  var wrench = require('wrench'),
-    util = require('util');
-  //-
-  var path        = require("path");
-  var fs          = require("fs");
+  var wrench = require('wrench');
+  var path = require("path");
+  var fs = require("fs");
   var crypto = require('crypto');
+  var ProgressBar = require('progress');
 
 
-  grunt.registerMultiTask("phantomizer-etags", "Asset's Etags generator for phantomizer app", function(){
-
+  grunt.registerMultiTask("phantomizer-etags",
+    "Generates apache/nginx etags headers for phantomizer app", function(){
 
     var options = this.options({
       src:"src/",
@@ -22,57 +21,37 @@ module.exports = function(grunt) {
       output:"output/"
     });
 
-    var eol = "\n";
-    var tab = "\t";
-
     var async = this.async();
     var finish = function(){
       grunt.log.ok();
-      async(true)
-    }
-    var files_etags = {};
-    var export_apache = function(files_etags,output_file){
-      var output = "";
-      for( var file in files_etags ){
-        var url = files_etags[file].url;
-        var etag = files_etags[file].etag;
-
-        url = url.substr(0,1)=="/"?url:"/"+url;
-
-        output += '<Location "'+url+'">' + eol;
-        output += tab+'Header set ETag "'+etag+'"' + eol;
-        output += '</Location>' + eol;
-      }
-      if( fs.existsSync(output_file) ) fs.unlinkSync(output_file)
-      fs.writeFileSync(output_file, output)
-    }
-    var export_nginx = function(files_etags,output_file){
-      var output = "";
-      for( var file in files_etags ){
-        var url = files_etags[file].url;
-        var etag = files_etags[file].etag;
-
-        url = url.substr(0,1)=="/"?url:"/"+url;
-
-        output += 'location '+url+' {' + eol;
-        output += tab+'add_header ETag '+etag+'' + eol;
-        output += '}' + eol;
-      }
-      if( fs.existsSync(output_file) ) fs.unlinkSync(output_file)
-      fs.writeFileSync(output_file, output)
+      async(true);
     }
 
-    wrench.mkdirSyncRecursive(options.output, "0777");
-    var curFiles = wrench.readdirSyncRecursive( options.src );
-    var curLength = 0;
+      grunt.file.mkdir(options.output);
+      var curFiles = wrench.readdirSyncRecursive( options.src );
+      var curLength = 0;
+      var files_etags = {};
+
+
+// initialize a progress bar
+      var bar = new ProgressBar(' done=[:current/:total] elapsed=[:elapseds] sprint=[:percent] eta=[:etas] [:bar]', {
+        complete: '#'
+        , incomplete: '-'
+        , width: 80
+        , total: curFiles.length
+      });
+
+
+
     for( var n in curFiles ){
       var f = options.src+curFiles[n];
-      if( fs.statSync(f).isFile() ){
+      if( grunt.file.exists(f) ){
         sha1_file(options.src, curFiles[n],function(relfile, f, d){
           files_etags[f] = {
             url:relfile,
             etag:d
           };
+          bar.tick();
           curLength++;
           if( curLength == curFiles.length ){
             for( var n in options.format){
@@ -86,19 +65,56 @@ module.exports = function(grunt) {
           }
         })
       }else{
+        bar.tick();
         curLength++;
       }
     }
 
   });
 
+  function export_apache(files_etags,output_file){
+
+    var eol = "\n";
+    var tab = "\t";
+    var output = "";
+
+    for( var file in files_etags ){
+      var url = files_etags[file].url;
+      var etag = files_etags[file].etag;
+
+      url = url.substr(0,1)=="/"?url:"/"+url;
+
+      output += '<Location "'+url+'">' + eol;
+      output += tab+'Header set ETag "'+etag+'"' + eol;
+      output += '</Location>' + eol;
+    }
+    grunt.file.write(output_file, output);
+  }
+  function export_nginx(files_etags,output_file){
+
+    var eol = "\n";
+    var tab = "\t";
+    var output = "";
+
+    for( var file in files_etags ){
+      var url = files_etags[file].url;
+      var etag = files_etags[file].etag;
+
+      url = url.substr(0,1)=="/"?url:"/"+url;
+
+      output += 'location '+url+' {' + eol;
+      output += tab+'add_header ETag '+etag+'' + eol;
+      output += '}' + eol;
+    }
+    grunt.file.write(output_file, output);
+  }
   function sha1_file(base_url, relfile, then){
 // change the algo to sha1, sha256 etc according to your requirements
     var algo = 'sha1';
     var shasum = crypto.createHash(algo);
 
     var file = base_url+relfile;
-    var enc = file.match("[.](css|html|htm)")?'utf8':null;
+    var enc = file.match("[.](css|html|htm|js|txt|appcache|manifest|xml|json)")?'utf8':null;
     fs.readFile(file, enc, function (err,data) {
       if (err) {
         return console.log(err);
